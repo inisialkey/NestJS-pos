ca022a13335f29199fbfcc209eaf781cb138191f
okidorvel <okidorvel404@gmail.com>
2022-08-04 14:21:02 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center">Back-end sistem penjualan sederhana dengan framework NestJs</p>
+
+## Description
+
+Repository dari tutorial [Membangun Back-End API sistem penjualan (POS) dengan NestJs (MySql, JWT, Swagger)](https://youtu.be/WuGKMQpVQRA)
+
+## Installation
+
+```bash
+$ npm install
+$ npm run start:dev
+```
+
+<p>
+Semoga bermanfaat
+</p>
---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
+import { ExistValidator } from './etc/validator/exist-validator';
+import { UniqueValidator } from './etc/validator/unique-validator';
+import { AuthModule } from './auth/auth.module';
+import { ProdukModule } from './produk/produk.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASS,
+      database: process.env.MYSQL_DB,
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+    AuthModule,
+    ProdukModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService, ExistValidator, UniqueValidator],
+})
+export class AppModule {}
